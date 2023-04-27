af35352f4b8baf7ca994f660ea5d473f16358acb
SamathaPendru <130585802+SamathaPendru@users.noreply.github.com>
2023-04-27 23:40:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
 streamlit.header("View Our Fruit List-Add Your Favourites!")
 #Snowflake-related functions
 def get_fruit_load_list():
-   with my_cnr.cursor() as my_cur:
+   with my_cur.cursor() as my_cur:
       my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
       return my_Cur.fetchall()
 #Add button to load the fruit
