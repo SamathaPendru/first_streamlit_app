68abd7dbeeb803d16ebb2b86c0ae473fa1b7442f
SamathaPendru <130585802+SamathaPendru@users.noreply.github.com>
2023-06-13 15:11:12 +0530

    added Emojis,one header,one item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parents Healthier Diner')
 
 streamlit.header('BreakFast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale,spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗kale,spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
